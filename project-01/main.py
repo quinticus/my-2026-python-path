d61a3afca193e9c5fbbc3f1b49a9c877b2a0dd00
Quinticus <bquinte2007@gmail.com>
2026-01-03 15:32:40 -0500

    "Added" Image Color Inversion
    Adding the ability for users to add images to be inverted and rewrote the code to be a lot more structured. Still doesn't work though...

--- project-01/main.py
+++ project-01/main.py
@@ -1,51 +1,116 @@
 from tkinter import *
 from tkinter import colorchooser
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import ImageTk, Image, ImageChops
 
-selected_color = ()
-inverted_color = ()
+# ------------- #
+# --- LOGIC --- #
+# ------------- #
 
-def invert_color():
-  global selected_color
-  global inverted_color
-  inverted_r = 255 - selected_color[0]
-  inverted_g = 255 - selected_color[1]
-  inverted_b = 255 - selected_color[2]
+def show_info(original, inverted, mode):
+  left_text = Label(
+    root,
+    text=None
+  )
+  left_text.grid(row=2, column=0)
+  left_output = Label(
+    root,
+    text=None
+  )
+  left_output.grid(row=3, column=0)
+
+  right_text = Label(
+    root,
+    text=None
+  )
+  right_text.grid(row=2, column=2)
+  right_output = Label(
+    root,
+    text=None
+  )
+  right_output.grid(row=3, column=2)
+
+  if mode == 'color':
+    pass
+  elif mode == 'image':
+    tk_image = ImageTk.PhotoImage(original)
+    tk_inverted = ImageTk.PhotoImage(inverted)
+
+def invert_color(color):
+  print(f"User is now inverting color ({color})")
+  inverted_r = 255 - color[0]
+  inverted_g = 255 - color[1]
+  inverted_b = 255 - color[2]
   inverted_color = (inverted_r, inverted_g, inverted_b)
-  show_invert.config(text=f"Inverted Color: {inverted_color}")
+  print(f"Color has now been converted ({inverted_color}).")
+  return inverted_color
 
-def get_color():
-  global selected_color
-  color_code = colorchooser.askcolor(title='Color to be inverted')
-  selected_color = color_code[0]
-  show_color.config(text=f'Selected Color: {selected_color}')
-  invert_color()
+def pick_color():
+  print("User is selecting color.")
+  selected_color = colorchooser.askcolor(title="Color to be Inverted")[0]
+  print(f"User selected color {selected_color}.")
+  inverted_color = invert_color(selected_color)
+  show_info(selected_color, inverted_color, 'color')
+
+def invert_image(image):
+  print(f"Image is now being inverted ({image}).")
+  opened_image = Image.open(image)
+  print(f"Image has now been opened ({opened_image}).")
+  inverted_image = ImageChops.invert(opened_image)
+  print(f"Image has now been inverted ({inverted_image}).")
+
+  return inverted_image
+
+def get_image():
+  print(f"User chose to send image.")
+  filetypes = (
+    ('.PNG Image', '*.png'),
+    ('.JPEG Image', '*.jpeg'),
+    ('.WEBP Image', '*.webp') # fuck you webp is ass
+  )
+
+  image = filedialog.askopenfilename(
+    title='Image to be inverted',
+    initialdir='/',
+    filetypes=filetypes
+  )
+  print(f"User picked image {image}")
+  inverted_image = invert_image(image)
+  show_info(image, inverted_image, 'image')
+
+# -------------- #
+# --- WINDOW --- #
+# -------------- #
 
 root = Tk()
-root.title("Color Inverter")
+root.title("Color Picker")
 
-label = Label(
+title = Label(
   root,
-  text='Use the buttton below to pick a color.'
+  text='Color Picker'
 )
-label.grid(row=0, column=0)
+title.grid(row=0, column=1)
 
-color_button = Button(
+color_button = ttk.Button(
   root,
-  text='Select Color',
-  command=get_color
+  text='Pick Color',
+  command=pick_color
 )
 color_button.grid(row=1, column=0)
 
-show_color = Label(
+or_label = Label(
   root,
-  text=None
+  text='or'
 )
-show_color.grid(row=2, column=0)
+or_label.grid(row=1, column=1)
 
-show_invert = Label(
+image_button = ttk.Button(
   root,
-  text=None
+  text='Upload Image',
+  command=get_image
 )
-show_invert.grid(row=3, column=0)
+image_button.grid(row=1, column=2)
+
 
 root.mainloop()
